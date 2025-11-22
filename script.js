e40e47bc202fddd092223d894c1d30645283e3c0
Ainsteen Varghese <88553740+ainsteen01@users.noreply.github.com>
2025-11-22 08:59:05 +0530

    Add files via upload
    Updation for mobile devices

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 // Simple fade-in on scroll
 const sections = document.querySelectorAll('.section');
+const mobileMenu = document.querySelector('.mobile-menu');
+const mobileMenuBtn = document.querySelector('.mobile-menu-btn');
+
+mobileMenuBtn.addEventListener('click', function(e) {
+  e.stopPropagation();
+  mobileMenu.style.display = 
+    mobileMenu.style.display === "flex" ? "none" : "flex";
+});
+
+document.addEventListener('click', (e) => {
+  if (e.target !== mobileMenuBtn) {
+    mobileMenu.style.display = "none";
+  }
+});
 
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
